6ea2231704ea5b5512d754cd712ce27d96c0bd79
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2025-02-02 14:48:13 +0300

    a few changes

--- scripts/ps_install.sh
+++ scripts/ps_install.sh
@@ -2,11 +2,14 @@
 
 source ./scripts/paths.sh
 
-installArchiveFunc() {
+installArchivesFunc() {
+
+}
+
+installPSFunc() {
     local ARCHIVE="https://iusearchbtw.isgood.host/files/photoshop.tar.xz"
     local ARCHIVE_NAME="photoshop.tar.xz"
 
-    #
     show_message_info "Download the archive. It may take time"
     curl -o "$HOME/Загрузки/$ARCHIVE_NAME" "$ARCHIVE"
 
@@ -22,8 +25,4 @@ installArchiveFunc() {
     tar -xJvf "$HOME/Загрузки/Adobe.tar.xz" -C "$HOME/Загрузки/"
     mv "$HOME/Загрузки/Adobe" "/home/daniil/photoshop/drive_c/users/daniil/AppData/Roaming/"
     WINEPREFIX=~/photoshop wine "/home/daniil/photoshop/drive_c/Program Files (x86)/photoshop/Photoshop.exe"
-}
-
-installPSFunc() {
-    installArchiveFunc
 }
