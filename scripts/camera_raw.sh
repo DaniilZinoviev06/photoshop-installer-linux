f3d9f502e0e19694e8a9f8a94fb80d1176c456c6
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2025-02-07 16:26:31 +0300

    Camera raw

--- scripts/camera_raw.sh
+++ scripts/camera_raw.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+
+
+check_ps() {
+
+}
+
+cr_installer() {
+    check_ps
+
+    sleep 5
+
+    show_message_info "OK. Starting to download the Camera Raw plugin..."
+
+    sleep 5
+
+    installArchiveFunc "$ARCHIVE_NAME_CR" "$ARCHIVE_CR" "$CR_SHA256"
+}
